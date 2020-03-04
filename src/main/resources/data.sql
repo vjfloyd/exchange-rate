87b8665d85cbc2cea288000dca91fbd899c08aa7
victordev <jonathan.rojas@tismart.com>
2020-03-03 23:09:44 -0500

    change with jean

---
+++ src/main/resources/data.sql
@@ -0,0 +1,8 @@
+DROP TABLE IF EXISTS billionaires;
+
+CREATE TABLE exchangerate (
+  code VARCHAR(250) NOT NULL PRIMARY KEY,
+  toCurrency VARCHAR(250) NOT NULL,
+  fromCurrency VARCHAR(250) NOT NULL,
+  valueToday DECIMAL DEFAULT NULL
+);
